05a9b1e2f99b1dc70d1b1522db004e33ddc56985
erickmo <erick.mo@vortege.com>
2015-04-01 22:19:58 +0700

    Fix folder case

---
+++ src/jsend/jsend.php
@@ -0,0 +1,41 @@
+<?php namespace ThunderID\jsend;
+
+class Jsend {
+	
+	protected $results;
+
+	function __construct($status, $data) 
+	{
+		// validate status
+		switch (strtolower($status))
+		{
+			case 'success': case 'error': case 'fail':
+				$status = strtolower($status);
+				break;
+			default:
+				throw new Exception("Invalid JSend Status (success or error or fail expected, ".$status." found)", 1);
+		}
+
+		// create results
+		$this->results['status'] = $status;
+		switch ($status)
+		{
+			case 'success': case 'fail':
+				$this->results['data'] = $data;
+				break;
+			case 'error':
+				$this->results['message'] = $data;
+				break;
+		}
+	}
+
+	function __toString()
+	{
+		return json_encode($this->results);
+	}
+
+	function toJson()
+	{
+		return json_encode($this->results);
+	}
+}
